9e37676853bff4b0713a74fb52deb78e4ec970c2
Bryan Robinson <bryanlrobinson@gmail.com>
2019-04-01 14:54:03 -0500

    Initial recommit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(config) {
+   
+    config.addCollection('posts', collection => {
+      return collection.getFilteredByGlob('posts/*.md');
+    });
+
+    config.addFilter("slice", require("./filters/slice.js"));
+    config.addFilter("lookup", require("./filters/lookup.js"));
+    config.addPassthroughCopy("images");
+    config.addPassthroughCopy("./admin");
+
+    return {
+        dir: {
+          input: ".",
+          output: "dist",
+          includes: "templates"
+        },
+        templateFormats : ["html", "md", "css"],
+        htmlTemplateEngine : "liquid",
+        markdownTemplateEngine : "liquid"
+      };
+}
---
+++ posts/welcome-post.md
@@ -0,0 +1,8 @@
+---
+title: "Notes from March 26, 2019"
+date: "2019-03-26"
+---
+
+## This is an h2
+
+This is our paragraph and **it has bold**
